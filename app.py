549a986c356376c94e847953d1cf7b6ac1a1aaec
Iwona <iwonawintrowicz@gmail.com>
2023-07-09 23:17:03 +0200

    Adding templates with 404 and 500 errors

--- app.py
+++ app.py
@@ -11,13 +11,11 @@ def index():
 @app.route('/teksty')
 def teksty():
     return render_template('teksty.html', the_title='Przeglądaj i edytuj bazę tekstów!', the_tactive_menu='teksty')
-    
 
 @app.route('/grain')
 def grain():
     return render_template('grain.html', the_title='Wybierz tekst i zagraj!', active_menu='grain')
     
-
 @app.route('/graout', methods=['GET','POST'])
 def graout():
     if request.method=="POST":
@@ -28,5 +26,15 @@ def graout():
 @app.route('/historia')
 def historia():
     return render_template('historia.html', the_title='Przejrzyj swoje podejścia!', active_menu='historia')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('404.html'), 500
+
+
 if __name__ == "__main__":
     app.run()
