76d1997374a27096deb44951ea2f098ee1f324ee
Iwona <iwonawintrowicz@gmail.com>
2023-07-10 19:44:50 +0200

    Adding records (time and amount of words to guess) into DB

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+
+
+class NameForm(FlaskForm):
+    tekst = StringField('Tu wklej tekst np. piosenki, wiersza, itp.', validators=[DataRequired()])
+    submit = SubmitField('Wyślij')
 
 class Teksty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,11 +28,20 @@ class Teksty(db.Model):
    
 
     def __repr__(self):
-        return 'Teksty: {}/{}'.format(self.id, self.name)
-
+        return 'Teksty: {}/{}'.format(self.id, self.text)
+    
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html', the_title='Dodaj tekst!', active_menu='home', the_current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        teksty = Teksty.query.filter_by(text=form.tekst.data).first()
+        if teksty is None:
+            teksty = Teksty(text=form.tekst.data, data=datetime.utcnow(), ilosc=len(set((form.tekst.data).split(' '))))
+            db.session.add(teksty)
+            db.session.commit()
+        session['tekst'] = form.tekst.data
+        return redirect(url_for('index'))
+    return render_template('index.html', the_form=form,the_name=session.get('tekst'), the_title='Dodaj tekst!', active_menu='home')
 
 @app.route('/teksty')
 def teksty():
